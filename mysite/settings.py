4be391e0789aca64c57c3583112e14bfcbc84b9e
rigarashi <wwwxx@icloud.com>
2021-05-01 16:41:35 +0900

    Changed the HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_GIRLS_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com','rigarashi.pythonanywhere.com']
 
 
 # Application definition
